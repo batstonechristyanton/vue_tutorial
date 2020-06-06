14b8ca0b11155ac53c2f981b977ff3b14b88cfdf
unknown <bchristyanton@trentu.ca>
2020-06-06 18:36:35 -0400

    changed files

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
